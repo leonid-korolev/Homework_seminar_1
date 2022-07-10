15e91621256ed9c3f4f2acaa662f3761dd57da5b
Leonid <leonkor64@mail.ru>
2022-07-11 01:26:01 +0300

    Add Task_2

---
+++ Task_2.py
@@ -0,0 +1,8 @@
+print('¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z')
+print()
+print('x y z')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(x,y,z)
+            print((int(not x or y or z)) == (not x and not y and not z)) 
