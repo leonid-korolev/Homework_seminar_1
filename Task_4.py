3dd685bab20caac3cf42fbee13dc9860f8933f3a
Leonid <leonkor64@mail.ru>
2022-07-11 01:29:26 +0300

    Add Task_4

---
+++ Task_4.py
@@ -0,0 +1,11 @@
+print('Введите номер четверти координатной плоскости')
+N = input()
+if N == '1' or N == 'I':
+    print(f' x ∈ ]0,+ထ [,  y ∈ ]0,+ထ [')
+elif N == '2' or N == 'II':
+    print(f' x ∈ ]0,-ထ [,  y ∈ ]0,+ထ [')
+elif N == '3' or N == 'III':
+    print(f' x ∈ ]0,-ထ [,  y ∈ ]0,-ထ [')
+elif N == '4' or N == 'IV':
+    print(f' x ∈ ]0,+ထ [,  y ∈ ]0,-ထ [')
+print()    
